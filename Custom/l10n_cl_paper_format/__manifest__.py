b1670854d134eb602a1b2e702fb7aa29224833e8
Alexander Olivares <aolivares@konos.cl>
2021-09-15 14:25:32 -0300

    [ADD] Wisecom: l10n_cl_paper_format module

---
+++ Custom/l10n_cl_paper_format/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Paper Format",
+
+    'summary': """
+        Paper format to be used by default on Chilean layouts
+    """,
+
+    'author': "Konos Soluciones & Servicios",
+    'website': "https://www.konos.cl",
+
+    'category': 'Localization/Chile',
+    'version': '0.2',
+
+    'depends': [
+        'base',
+    ],
+
+    'data': [
+        'data/report_paperformat_data.xml',
+    ],
+
+    'demo': [
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
